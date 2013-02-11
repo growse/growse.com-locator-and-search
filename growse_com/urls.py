e0bb9af367de138b32557a1003083ae22bd382a9
Andrew Rowson <github@growse.com>
2013-02-11 18:30:31 +0000

    Some styling tweaks

--- growse_com/urls.py
+++ growse_com/urls.py
@@ -12,24 +12,17 @@ sitemaps = {
 }
 
 urlpatterns = patterns('',
-    (r'^robots\.txt$', direct_to_template, {'template': 'robots.txt', 'mimetype': 'text/plain'}),
-    (r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
-    (r'^cp/', include(admin.site.urls)),
-    (r'^news/rss/$', RssFeed()),
-    (r'^news/comments/(?P<article_shorttitle>.+)/$', 'growse_com.blog.views.article'),
-    (r'^projects/(?P<article_shorttitle>.+)/$', 'growse_com.blog.views.article'),
-    (r'^misc/(?P<article_shorttitle>.+)/$', 'growse_com.blog.views.article'),
-    (r'videos/(?P<article_shorttitle>.+)/$', 'growse_com.blog.views.article'),
-    (r'^news/$', 'growse_com.blog.views.newsindex'),
-    (r'^photos/$', 'growse_com.blog.views.photos'),
-    (r'^projects/$', 'growse_com.blog.views.projectsindex'),
-    (r'^videos/$', 'growse_com.blog.views.videosindex'),
-    (r'^misc/$', 'growse_com.blog.views.miscindex'),
-    (r'^links/$', 'growse_com.blog.views.links'),
-    (r'^search/(?P<searchterm>.+)/(?P<page>\d+)/$', 'growse_com.blog.views.search'),
-    (r'^search/(?P<searchterm>.+)/$', 'growse_com.blog.views.search'),
-    (r'^search/$', 'growse_com.blog.views.search'),
-    (r'^$', 'growse_com.blog.views.article'),
-)
+                      (r'^robots\.txt$', direct_to_template, {'template': 'robots.txt', 'mimetype': 'text/plain'}),
+                      (r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
+                      (r'^cp/', include(admin.site.urls)),
+                      (r'^news/rss/$', RssFeed()),
+                      (r'^news/comments/(?P<article_shorttitle>.+)/$', 'growse_com.blog.views.article'),
+                      (r'^news/$', 'growse_com.blog.views.article'),
+                      (r'^photos/$', 'growse_com.blog.views.photos'),
+                      (r'^search/(?P<searchterm>.+)/(?P<page>\d+)/$', 'growse_com.blog.views.search'),
+                      (r'^search/(?P<searchterm>.+)/$', 'growse_com.blog.views.search'),
+                      (r'^search/$', 'growse_com.blog.views.search'),
+                      (r'^$', 'growse_com.blog.views.article'),
+                    )
 
 urlpatterns += staticfiles_urlpatterns()

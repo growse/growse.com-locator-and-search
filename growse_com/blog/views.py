96e2f1af586c00dfb503ffd4d19c69e3ac746227
Andrew Rowson <growse@github.com>
2013-05-26 10:38:15 +0100

    Added controller methods for fuzzy date matching. Rewrite urls

--- growse_com/blog/views.py
+++ growse_com/blog/views.py
@@ -2,10 +2,27 @@ from django.core.mail import send_mail
 from django.template import RequestContext
 from django.shortcuts import get_object_or_404, redirect, render_to_response
 from django.db.models import Count
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
+import datetime
 from growse_com.blog.models import Article
 from growse_com.blog.models import Comment
+
+
+def article_shorttitle(request, article_shorttitle=''):
+    article = get_object_or_404(Article, shorttitle=article_shorttitle)
+    articledate = article.datestamp.date()
+    return redirect('/' + str(articledate.year) + '/' + str(articledate.month).zfill(2) + '/' + str(
+        articledate.day).zfill(2) + '/' + article.shorttitle + '/')
+
+
+def article_bydate(request, year, month='01', day='01'):
+    requesteddate = datetime.datetime(int(year), int(month), int(day))
+    article = get_object_or_404(Article, datestamp=requesteddate)
+    articledate = article.datestamp.date()
+    return redirect('/' + str(articledate.year) + '/' + str(articledate.month).zfill(2) + '/' + str(
+        articledate.day).zfill(2) + '/' + article.shorttitle + '/')
+
 
 def article(request, article_shorttitle=''):
     c = RequestContext(request)
@@ -19,31 +36,28 @@ def article(request, article_shorttitle=''):
         comment = request.POST.get('comment')
         spamfilter = request.POST.get('email')
         if spamfilter is None or len(spamfilter) == 0:
-            Comment.objects.create(name=name, website=website, comment=comment, article=article, ip=request.META['REMOTE_ADDR'])
+            Comment.objects.create(name=name, website=website, comment=comment, article=article,
+                                   ip=request.META['REMOTE_ADDR'])
             try:
-                send_mail('New Comment on growse.com', 'Someone posted a comment on growse.com. Over at http://www.growse.com/news/comments/' + article.shorttitle + '/', 'hubfour@growse.com', ['comments@growse.com'], fail_silently=False)
+                send_mail('New Comment on growse.com',
+                          'Someone posted a comment on growse.com. Over at http://www.growse.com/news/comments/' + article.shorttitle + '/',
+                          'hubfour@growse.com', ['comments@growse.com'], fail_silently=False)
             except:
                 pass
         return redirect("/news/comments/" + article_shorttitle + "/")
     else:
         articlenavlist = Article.objects.all().order_by('-datestamp')
-        prevarticle = None
-        nextarticle = None
-        if article.type == 'NEWS':
-            prevarticle = Article.objects.filter(type='NEWS').filter(datestamp__lt=article.datestamp).order_by("-datestamp")[0:1]
-            nextarticle = Article.objects.filter(type='NEWS').filter(datestamp__gt=article.datestamp).order_by("datestamp")[0:1]
-            if prevarticle.count() != 0:
-                prevarticle = prevarticle[0]
-            if nextarticle.count() != 0:
-                nextarticle = nextarticle[0]
         comments = Comment.objects.filter(article__id=article.id).order_by("datestamp")
-        archives = Article.objects.filter(type='NEWS').extra(select={'month': "DATE_TRUNC('month',datestamp)"}).values('month').annotate(Count('title')).order_by('-month')
+        archives = Article.objects.filter(type='NEWS').extra(select={'month': "DATE_TRUNC('month',datestamp)"}).values(
+            'month').annotate(Count('title')).order_by('-month')
         prevyear = None
         for archive in archives:
             if archive["month"].year != prevyear:
                 archive["newyear"] = True
                 prevyear = archive["month"].year
-        return render_to_response('blog/article.html', {'archives': archives, 'articlenavlist': articlenavlist, 'comments': comments, 'article': article, 'nav': article.type.lower()}, c)
+        return render_to_response('blog/article.html',
+                                  {'archives': archives, 'articlenavlist': articlenavlist, 'comments': comments,
+                                   'article': article, 'nav': article.type.lower()}, c)
 
 
 def search(request, searchterm=None, page=1):
@@ -52,17 +66,17 @@ def search(request, searchterm=None, page=1):
         if request.method == 'GET':
             return redirect("/", Permanent=True)
         if request.method == 'POST':
-            return redirect("/search/" + request.POST.get('a','') + "/")
+            return redirect("/search/" + request.POST.get('a', '') + "/")
     else:
-        results_list =  Article.objects.extra(select={'headline':"ts_headline(body,plainto_tsquery('english',%s))",'rank':"ts_rank(idxfti,plainto_tsquery('english',%s))"},where=["idxfti @@ plainto_tsquery('english',%s)"],params=[searchterm],select_params=[searchterm,searchterm]).order_by('-rank')
-        paginator = Paginator(results_list,10)
+        results_list = Article.objects.extra(select={'headline': "ts_headline(body,plainto_tsquery('english',%s))",
+                                                     'rank': "ts_rank(idxfti,plainto_tsquery('english',%s))"},
+                                             where=["idxfti @@ plainto_tsquery('english',%s)"], params=[searchterm],
+                                             select_params=[searchterm, searchterm]).order_by('-rank')
+        paginator = Paginator(results_list, 10)
         try:
             results = paginator.page(page)
         except(EmptyPage, InvalidPage):
             results = paginator.page(paginator.num_pages)
-        return render_to_response('blog/search.html',{'results':results,'searchterm':searchterm}, c)
+        return render_to_response('blog/search.html', {'results': results, 'searchterm': searchterm}, c)
 
-def links (request):
-    c=RequestContext(request);
-    return render_to_response('blog/links.html',{'nav':'links'},c)
 

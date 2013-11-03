f3014af53376e07f1de6a5b9fa0d1c16711f3fa0
Andrew Rowson <github@growse.com>
2013-11-03 17:03:35 +0000

    Updated requirements, added fabric file

---
+++ fabfile.py
@@ -0,0 +1,15 @@
+from fabric.api import sudo, cd, run, env, local, prefix
+
+env.hosts = ['109.123.84.240']
+env.user = 'growse'
+
+
+def deploy():
+    local('git rebase master deploy')
+    local('git push')
+    local('git checkout master')
+    with cd('/home/growse/django-sites/www.growse.com'):
+        with prefix('source bin/activate'):
+            run('git pull')
+            run('pip install --upgrade -r requirements.txt')
+            sudo('restart uwsgi')

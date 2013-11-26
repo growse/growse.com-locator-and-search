6c71eb177a5fa65b9a7a717be9044e1e6a7d41d3
Andrew Rowson <github@growse.com>
2013-11-26 20:50:42 +0000

    Fixed js scroll bug on chrome beta

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,57 +1,47 @@
-//WebFontConfig = {
-//    google: {
-//        families: ['Droid Sans', 'Open Sans:300,400','Quando']
-//    }
-//};
-
 $(function () {
-//    var wf = document.createElement('script');
-//    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-//    '://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js';
-//wf.type = 'text/javascript';
-//wf.async = 'true';
-//var s = document.getElementsByTagName('script')[0];
-//s.parentNode.insertBefore(wf, s);
-
-var percentagedown = ($('.here').position().top / $(window).height()) * 100;
-if (percentagedown > 50) {
-    $('nav').scrollTop($('.here').position().top - ($(window).height() / 2) + ($('nav ul li:first').height() / 2));
-}
-$('nav').bind('mousewheel DOMMouseScroll', function (e) {
-    var delta = e.wheelDelta || e.originalEvent.wheelDelta || -e.detail || -e.originalEvent.detail;
-    this.scrollTop += ( delta < 0 ? 1 : -1 ) * 40;
-    e.preventDefault();
-});
+    var percentagedown = ($('.here').position().top / $(window).height()) * 100;
+    if (percentagedown > 50) {
+        $('nav').scrollTop($('.here').position().top - ($(window).height() / 2) + ($('nav ul li:first').height() / 2));
+    }
+    $('nav').bind('mousewheel DOMMouseScroll', function (e) {
+        var delta = e.wheelDelta || e.originalEvent.wheelDelta || -e.detail || -e.originalEvent.detail;
+        this.scrollTop += ( delta < 0 ? 1 : -1 ) * 40;
+        //This gets around a bug in chrome beta where the nav doesn't rerender after scrollTop()
+        this.style.display = 'none';
+        this.offsetHeight;
+        this.style.display = 'block';
+        e.preventDefault();
+    });
 });
 
 var growse = function () {
     return {
         loadingNav: false,
-            loadNav: function () {
-                if (!growse.loadingNav) {
-                    growse.loadingNav = true;
-                    $.getJSON("/navlist/since/" + $("#articlenav > li:first").data('datestamp'), function (data) {
-                        $.each(data, function (i, v) {
-                            var markup = "<li><a href=\"/" + v.year + "/" + v.month + "/" + v.day + "/" + v.shorttitle + "/\" title=\"" + v.title + "\"<span>" + v.title + "</span></a></li>";
-                            $("#articlenav").prepend(markup);
-                        });
+        loadNav: function () {
+            if (!growse.loadingNav) {
+                growse.loadingNav = true;
+                $.getJSON("/navlist/since/" + $("#articlenav > li:first").data('datestamp'), function (data) {
+                    $.each(data, function (i, v) {
+                        var markup = "<li><a href=\"/" + v.year + "/" + v.month + "/" + v.day + "/" + v.shorttitle + "/\" title=\"" + v.title + "\"<span>" + v.title + "</span></a></li>";
+                        $("#articlenav").prepend(markup);
                     });
-                    $.getJSON("/navlist/before/" + $("#articlenav > li:last").data('datestamp'), function (data) {
-                        $.each(data, function (i, v) {
-                            var markup = "<li><a href=\"/" + v.year + "/" + v.month + "/" + v.day + "/" + v.shorttitle + "/\" title=\"" + v.title + "\"<span>" + v.title + "</span></a></li>";
-                            $("#articlenav").append(markup);
-                        });
+                });
+                $.getJSON("/navlist/before/" + $("#articlenav > li:last").data('datestamp'), function (data) {
+                    $.each(data, function (i, v) {
+                        var markup = "<li><a href=\"/" + v.year + "/" + v.month + "/" + v.day + "/" + v.shorttitle + "/\" title=\"" + v.title + "\"<span>" + v.title + "</span></a></li>";
+                        $("#articlenav").append(markup);
+                    });
 
-                    });
-                }
-            },
-            getLocation: function () {
-                $.getJSON("//res.growse.com/nocache/latitude.js", function (data) {
-                    var coords = data.features[0].geometry.coordinates[1] + ',' + data.features[0].geometry.coordinates[0];
-                    var url = '//maps.googleapis.com/maps/api/staticmap?markers=color:red|' + coords + '&zoom=13&size=285x200&sensor=false';
-                    $('#twitterlocation_div p').html("<a href=\"//maps.google.com?q=" + coords + "\"><img src=" + url + " /></a>");
                 });
             }
+        },
+        getLocation: function () {
+            $.getJSON("//res.growse.com/nocache/latitude.js", function (data) {
+                var coords = data.features[0].geometry.coordinates[1] + ',' + data.features[0].geometry.coordinates[0];
+                var url = '//maps.googleapis.com/maps/api/staticmap?markers=color:red|' + coords + '&zoom=13&size=285x200&sensor=false';
+                $('#twitterlocation_div p').html("<a href=\"//maps.google.com?q=" + coords + "\"><img src=" + url + " /></a>");
+            });
+        }
     };
 }();
 
@@ -72,7 +62,7 @@ function prettyDate(time) {
         return;
 
     return day_diff == 0 && (
-            diff < 60 && "just now" ||
+        diff < 60 && "just now" ||
             diff < 120 && "1 minute ago" ||
             diff < 3600 && Math.floor(diff / 60) + " minutes ago" ||
             diff < 7200 && "1 hour ago" ||
@@ -84,10 +74,10 @@ function prettyDate(time) {
 
 // If jQuery is included in the page, adds a jQuery plugin to handle it as well
 if (typeof jQuery != "undefined")
-jQuery.fn.prettyDate = function () {
-    return this.each(function () {
-        var date = prettyDate(this.title);
-        if (date)
-        jQuery(this).text(date);
-    });
-};
+    jQuery.fn.prettyDate = function () {
+        return this.each(function () {
+            var date = prettyDate(this.title);
+            if (date)
+                jQuery(this).text(date);
+        });
+    };

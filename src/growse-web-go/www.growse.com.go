c0d4eae46e2216ea41b2a7e930ca3b7c2cbf7ae9
Andrew Rowson <github@growse.com>
2014-12-15 18:01:59 +0000

    Updateeeed

--- src/growse-web-go/www.growse.com.go
+++ src/growse-web-go/www.growse.com.go
@@ -4,22 +4,16 @@ import (
 	"database/sql"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/julienschmidt/httprouter"
 	"github.com/lib/pq"
-	"github.com/oxtoacart/bpool"
 	"github.com/russross/blackfriday"
-	"html/template"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"path/filepath"
 	"strings"
 	"time"
 )
 
 var (
-	templates          map[string]*template.Template
-	bufpool            *bpool.BufferPool
 	db                 *sql.DB
 	stylesheetname     string
 	javascriptfilename string
@@ -37,20 +31,6 @@ type Article struct {
 func loadLatestArticle() (*Article, error) {
 	var a Article
 	err := db.QueryRow(`Select id,datestamp,shorttitle,title,markdown from articles where published=true order by datestamp desc limit 1`).Scan(&a.Id, &a.Timestamp, &a.Slug, &a.Title, &a.Markdown)
-	switch {
-	case err == sql.ErrNoRows:
-		return nil, fmt.Errorf("No article found")
-	case err != nil:
-		return nil, err
-	default:
-		a.Rendered = (string)(blackfriday.MarkdownCommon(([]byte)(a.Markdown)))
-		return &a, nil
-	}
-}
-
-func loadArticle(slug string) (*Article, error) {
-	var a Article
-	err := db.QueryRow(`Select id,datestamp,shorttitle,title,markdown from articles where shorttitle=$1 and published=true`, slug).Scan(&a.Id, &a.Timestamp, &a.Slug, &a.Title, &a.Markdown)
 	switch {
 	case err == sql.ErrNoRows:
 		return nil, fmt.Errorf("No article found")
@@ -93,36 +73,6 @@ func LatestArticleHandler(c *gin.Context) {
 	c.HTML(200, "base.tmpl", obj)
 }
 
-func ArticleHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-
-}
-
-func (article *Article) GetAbsoluteUrl() string {
-	return ""
-}
-
-func RenderTemplate(w http.ResponseWriter, name string, data map[string]interface{}) error {
-	tmpl, ok := templates[name]
-	if !ok {
-		return fmt.Errorf("The template %s was not found", name)
-	}
-	buf := bufpool.Get()
-	err := tmpl.ExecuteTemplate(buf, "base", data)
-	if err != nil {
-		bufpool.Put(buf)
-		return err
-	}
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	buf.WriteTo(w)
-	bufpool.Put(buf)
-	return nil
-}
-
-func init() {
-	bufpool = bpool.NewBufferPool(64)
-	templates = make(map[string]*template.Template)
-}
-
 func main() {
 	yay := pq.ListenerEventConnected
 	log.Print(yay)
@@ -141,7 +91,7 @@ func main() {
 	stylesheets, _ := ioutil.ReadDir(staticPath + "css/")
 	var lastTime time.Time
 	for _, file := range stylesheets {
-		if !file.IsDir() && (lastTime == nil || file.ModTime() > lastTime) && strings.HasSuffix(file.Name(), ".www.css") {
+		if !file.IsDir() && (lastTime.IsZero() || file.ModTime().After(lastTime)) && strings.HasSuffix(file.Name(), ".www.css") {
 			lastTime = file.ModTime()
 			stylesheetname = file.Name()
 		}

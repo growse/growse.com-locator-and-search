c3fa33640bf2133f0326bb56e059b136df6620a5
Andrew Rowson <github@growse.com>
2014-12-22 13:33:37 +0000

    Progoogogressss

---
+++ src/growse-web-go/article.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/bradfitz/gomemcache/memcache"
+	"github.com/russross/blackfriday"
+	"html/template"
+	"time"
+)
+
+type Article struct {
+	Id        uint32
+	Timestamp time.Time
+	Slug      string
+	Title     string
+	Markdown  string
+}
+
+func (article *Article) getCacheKey() string {
+	return Truncate(fmt.Sprintf("growse.com-article-%d-%02d-%02d-%s", article.Timestamp.Year(), article.Timestamp.Month(), article.Timestamp.Day(), article.Slug), 250)
+}
+
+func getCacheKey(year int, month int, day int, slug string) string {
+	return Truncate(fmt.Sprintf("growse.com-article-%d-%02d-%02d-%s", year, month, day, slug), 250)
+}
+
+func (article *Article) cacheArticle() error {
+	articleAsJson, err := json.Marshal(article)
+	if err != nil {
+		return err
+	}
+	memcacheItem := memcache.Item{Key: article.getCacheKey(), Value: articleAsJson}
+	err = memcacheClient.Set(&memcacheItem)
+	return err
+}
+
+func getArticleFromCache(cacheKey string) (*Article, error) {
+	var article Article
+	articleFromCache, err := memcacheClient.Get(cacheKey)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(articleFromCache.Value, &article)
+	if err != nil {
+		return nil, err
+	}
+	return &article, nil
+}
+
+func GetArticle(year int, month int, day int, slug string) (*Article, error) {
+	cacheKey := getCacheKey(year, month, day, slug)
+	articleFromCache, err := getArticleFromCache(cacheKey)
+	if err == nil {
+		return articleFromCache, nil
+	}
+	var article Article
+	err = db.QueryRow(`Select id,datestamp,shorttitle,title,markdown from articles where published=true and shorttitle=$1 limit 1`, slug).Scan(&article.Id, &article.Timestamp, &article.Slug, &article.Title, &article.Markdown)
+	if err == nil {
+		article.cacheArticle()
+		return &article, nil
+	}
+	return nil, err
+}
+
+func (article *Article) GetAbsoluteUrl() string {
+	return fmt.Sprintf("/%d/%02d/%02d/%s", article.Timestamp.Year(), article.Timestamp.Month(), article.Timestamp.Day(), article.Slug)
+}
+
+func (article *Article) Rendered() template.HTML {
+	return template.HTML(blackfriday.MarkdownCommon(([]byte)(article.Markdown)))
+}
+
+func Truncate(input string, length int) string {
+	if len(input) < length {
+		return input
+	} else {
+		return input[:length]
+	}
+}
--- src/growse-web-go/www.growse.com.go
+++ src/growse-web-go/www.growse.com.go
@@ -2,75 +2,172 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"flag"
 	"fmt"
+	"github.com/bradfitz/gomemcache/memcache"
 	"github.com/gin-gonic/gin"
 	"github.com/lib/pq"
-	"github.com/russross/blackfriday"
 	"io/ioutil"
 	"log"
-	"path/filepath"
+	"os"
+	"path"
+	"strconv"
 	"strings"
 	"time"
 )
 
 var (
 	db                 *sql.DB
-	stylesheetname     string
+	templatePath       string
+	staticPath         string
+	stylesheetfilename string
 	javascriptfilename string
+	memcacheClient     *memcache.Client
 )
 
-type Article struct {
-	Id        int
-	Timestamp time.Time
-	Slug      string
-	Title     string
-	Markdown  string
-	Rendered  string
+type ArticleMonth struct {
+	Year  int
+	Month int
+	Count int
 }
 
 func loadLatestArticle() (*Article, error) {
-	var a Article
-	err := db.QueryRow(`Select id,datestamp,shorttitle,title,markdown from articles where published=true order by datestamp desc limit 1`).Scan(&a.Id, &a.Timestamp, &a.Slug, &a.Title, &a.Markdown)
-	switch {
-	case err == sql.ErrNoRows:
-		return nil, fmt.Errorf("No article found")
-	case err != nil:
-		return nil, err
-	default:
-		a.Rendered = (string)(blackfriday.MarkdownCommon(([]byte)(a.Markdown)))
-		return &a, nil
+	articleCacheKey, err := memcacheClient.Get("growse.com-latest")
+	var article Article
+	if err == nil {
+		articleFromCache, err := memcacheClient.Get(string(articleCacheKey.Value))
+		if err == nil {
+			json.Unmarshal(articleFromCache.Value, &article)
+		}
+	} else {
+		err := db.QueryRow(`Select id,datestamp,shorttitle,title,markdown from articles where published=true order by datestamp desc limit 1`).Scan(&article.Id, &article.Timestamp, &article.Slug, &article.Title, &article.Markdown)
+		switch {
+		case err == sql.ErrNoRows:
+			return nil, fmt.Errorf("No article found")
+		case err != nil:
+			return nil, err
+		default:
+			err := article.cacheArticle()
+			if err == nil {
+				memcacheClient.Set(&memcache.Item{Key: "growse.com-latest", Value: []byte(article.getCacheKey())})
+			}
+		}
 	}
+	return &article, nil
 }
 
-func loadIndex() (*[]Article, error) {
-	rows, err := db.Query("Select id, datestamp,shorttitle,title from articles where published=true order by datestamp desc;")
+func ArticleHandler(c *gin.Context) {
+	year, err := strconv.Atoi("2" + c.Params.ByName("year"))
 	if err != nil {
-		return nil, err
+		c.String(404, err.Error())
+		return
 	}
-	defer rows.Close()
-	var articles []Article
-	for rows.Next() {
-		var article Article
-		rows.Scan(&article.Id, &article.Timestamp, &article.Slug, &article.Title)
-		articles = append(articles, article)
+
+	month, err := strconv.Atoi(c.Params.ByName("month"))
+	if err != nil {
+		c.String(404, err.Error())
+		return
 	}
-	return &articles, nil
+
+	day, err := strconv.Atoi(c.Params.ByName("day"))
+	if err != nil {
+		c.String(404, err.Error())
+		return
+	}
+
+	slug := c.Params.ByName("slug")
+
+	article, err := GetArticle(year, month, day, slug)
+	if err != nil {
+		c.String(404, "404 Not Found")
+		return
+	}
+
+	index, months, err := loadIndex()
+	if err != nil {
+		c.String(500, err.Error())
+		return
+	}
+	obj := gin.H{"Index": index, "Title": article.Title, "Months": months, "Article": article, "Stylesheet": stylesheetfilename, "Javascript": javascriptfilename}
+
+	c.HTML(200, "article.tmpl", obj)
 }
 
 func LatestArticleHandler(c *gin.Context) {
 	article, err := loadLatestArticle()
 	if err != nil {
-		c.String(400, err.Error())
+		c.String(404, err.Error())
 		return
 	}
-	index, err := loadIndex()
+	index, months, err := loadIndex()
 	if err != nil {
 		c.String(500, err.Error())
 		return
 	}
-	obj := gin.H{"Index": index, "Title": "yaaaaay", "Article": article, "Stylesheet": stylesheetname, "Javascript": javascriptfilename}
+	obj := gin.H{"Index": index, "Title": article.Title, "Months": months, "Article": article, "Stylesheet": stylesheetfilename, "Javascript": javascriptfilename}
 
-	c.HTML(200, "base.tmpl", obj)
+	c.HTML(200, "article.tmpl", obj)
+}
+
+func loadIndex() (*[]Article, *[]ArticleMonth, error) {
+	indexFromCache, err := memcacheClient.Get("growse.com-index")
+	var articles []Article
+	if err != nil {
+
+		rows, err := db.Query("Select id, datestamp,shorttitle,title from articles where published=true order by datestamp desc;")
+		if err != nil {
+			return nil, nil, err
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			var article Article
+			rows.Scan(&article.Id, &article.Timestamp, &article.Slug, &article.Title)
+			articles = append(articles, article)
+		}
+		indexToCache, _ := json.Marshal(articles)
+		memcacheItem := memcache.Item{Key: "growse.com-index", Value: indexToCache}
+		memcacheClient.Set(&memcacheItem)
+	} else {
+		json.Unmarshal(indexFromCache.Value, &articles)
+	}
+
+	monthRows, err := db.Query("select date_part('year',datestamp) as year, date_part('month',datestamp) as month, count(*) as count  from articles group by date_part('year',datestamp), date_part('month',datestamp) order by year asc, month asc")
+	if err != nil {
+		return nil, nil, err
+	}
+	defer monthRows.Close()
+	var months []ArticleMonth
+	for monthRows.Next() {
+		var month ArticleMonth
+		monthRows.Scan(&month.Year, &month.Month, &month.Count)
+		months = append(months, month)
+
+	}
+	return &articles, &months, nil
+}
+
+func init() {
+	flag.StringVar(&templatePath, "templatePath", "", "The path to the templates")
+	flag.StringVar(&staticPath, "staticPath", "", "The path to the static files")
+
+	flag.Parse()
+
+	if templatePath == "" {
+		log.Fatalf("No template directory supplied")
+	}
+	if staticPath == "" {
+		log.Fatalf("No static directory supplied")
+	}
+	if _, err := os.Stat(templatePath); os.IsNotExist(err) {
+		log.Fatalf("No such file or directory: %s", templatePath)
+
+	}
+	if _, err := os.Stat(staticPath); os.IsNotExist(err) {
+		log.Fatalf("No such file or directory: %s", staticPath)
+	}
+
 }
 
 func main() {
@@ -84,23 +181,41 @@ func main() {
 
 	router := gin.Default()
 	gin.SetMode(gin.ReleaseMode)
-	staticPath := "/Users/andrew/Projects/growse-web-go/static/"
-	router.LoadHTMLTemplates("/Users/andrew/Projects/growse-web-go/templates/*.tmpl")
+	router.LoadHTMLTemplates(path.Join(templatePath, "*.tmpl"))
 	router.Static("/static/", staticPath)
 	//Get latest updated stylesheet
-	stylesheets, _ := ioutil.ReadDir(staticPath + "css/")
-	var lastTime time.Time
+	stylesheets, _ := ioutil.ReadDir(path.Join(staticPath, "css"))
+	var lastTimeCss time.Time
 	for _, file := range stylesheets {
-		if !file.IsDir() && (lastTime.IsZero() || file.ModTime().After(lastTime)) && strings.HasSuffix(file.Name(), ".www.css") {
-			lastTime = file.ModTime()
-			stylesheetname = file.Name()
+		if !file.IsDir() && (lastTimeCss.IsZero() || file.ModTime().After(lastTimeCss)) && strings.HasSuffix(file.Name(), ".www.css") {
+			lastTimeCss = file.ModTime()
+			stylesheetfilename = file.Name()
 		}
 	}
+	if stylesheetfilename == "" {
+		log.Fatal("No stylesheet found in staticpath. Perhaps run Grunt first?")
+	}
 
-	javascripts, _ := filepath.Glob(staticPath + "js/*.www.js")
-	javascriptfilename = "/static/js/" + filepath.Base(javascripts[0])
+	javascripts, _ := ioutil.ReadDir(path.Join(staticPath, "js"))
+	var lastTimeJs time.Time
+
+	for _, file := range javascripts {
+		if !file.IsDir() && (lastTimeJs.IsZero() || file.ModTime().After(lastTimeJs)) && strings.HasSuffix(file.Name(), ".www.js") {
+			lastTimeJs = file.ModTime()
+			javascriptfilename = file.Name()
+		}
+	}
+	if javascriptfilename == "" {
+		log.Fatal("No javascript found in staticpath. Perhaps run Grunt first?")
+	}
+
+	//Caching time
+	memcacheClient = memcache.New("/tmp/memcache.sock")
+
+	//Ugly hack to deal with the fact that httprouter can't cope with both /static/ and /:year existing
+	//All years will begin with 2. So this sort of helps. Kinda.
+	router.GET("/2:year/:month/:day/:slug", ArticleHandler)
 	router.GET("/", LatestArticleHandler)
-	//router.GET("/:year/:month/:day/:slug", ArticleHandler)
 	router.Run(":8080")
 	db.Close()
 }

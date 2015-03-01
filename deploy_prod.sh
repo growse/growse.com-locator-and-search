6d55716be336c0092dafa4fb1cac9529f8055286
Andrew Rowson <github@growse.com>
2015-03-01 20:48:00 +0000

    Faster deploy

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,3 +1,2 @@
-ssh www-data@www.growse.com "sudo supervisorctl stop www.growse.com-golang"
-scp src/growse-web-go/growse-web-go www-data@www.growse.com:/var/www/growse-web-go
-ssh www-data@www.growse.com "cd /var/www/growse-web-src/ && git pull && cd static && grunt && sudo supervisorctl start www.growse.com-golang"
+scp src/growse-web-go/growse-web-go www-data@www.growse.com:/tmp/growse-web-go
+ssh www-data@www.growse.com "sudo supervisorctl stop www.growse.com-golang && mv /tmp/growse-web-go /var/www/growse-web-go/growse-web-go && cd /var/www/growse-web-src/ && git pull && cd static && grunt && sudo supervisorctl start www.growse.com-golang"

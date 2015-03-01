e47b8a343df6f37945da2c0991744e200bb164b7
Andrew Rowson <github@growse.com>
2015-03-01 20:45:05 +0000

    deploy scripts now does static updating

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,3 +1,3 @@
 ssh www-data@www.growse.com "sudo supervisorctl stop www.growse.com-golang"
 scp src/growse-web-go/growse-web-go www-data@www.growse.com:/var/www/growse-web-go
-ssh www-data@www.growse.com "sudo supervisorctl start www.growse.com-golang"
+ssh www-data@www.growse.com "cd /var/www/growse-web-src/ && git pull && cd static && grunt && sudo supervisorctl start www.growse.com-golang"

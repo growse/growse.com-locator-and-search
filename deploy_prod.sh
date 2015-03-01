04fb897c851af2625068b847d07afe4543e5a42d
Andrew Rowson <github@growse.com>
2015-03-01 20:35:53 +0000

    Fixed ssh user on deploy script

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,3 +1,3 @@
-ssh www.growse.com "sudo supervisorctl stop www.growse.com-golang"
+ssh www-data@www.growse.com "sudo supervisorctl stop www.growse.com-golang"
 scp src/growse-web-go/growse-web-go www-data@www.growse.com:/var/www/growse-web-go
-ssh www.growse.com "sudo supervisorctl start www.growse.com-golang"
+ssh www-data@www.growse.com "sudo supervisorctl start www.growse.com-golang"

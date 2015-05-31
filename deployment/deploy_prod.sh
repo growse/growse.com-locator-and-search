9d4681da4e44d564196913c7063e8ecb7b68a555
Andrew Rowson <github@growse.com>
2015-05-31 16:53:23 +0100

    Restructed for intellij

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,2 +1,2 @@
-scp src/growse-web-go/growse-web-go www-data@www.growse.com:/tmp/growse-web-go
+scp growse-web-go www-data@www.growse.com:/tmp/growse-web-go
 ssh www-data@www.growse.com "sudo supervisorctl stop www.growse.com-golang && mv /tmp/growse-web-go /var/www/growse-web-go/growse-web-go && cd /var/www/growse-web-src/ && git pull && cd static && grunt && sudo supervisorctl start www.growse.com-golang"

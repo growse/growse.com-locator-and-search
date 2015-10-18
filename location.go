ff276154477266cf1357c1dee8c799f60dbad487
growse <github@growse.com>
2015-10-18 13:52:02 +0100

    Better error on bad location

--- location.go
+++ location.go
@@ -167,7 +167,7 @@ func LocatorHandler(c *gin.Context) {
 
 	newLocation := false
 	for _, locator := range locators {
-		locator.DeviceTimestamp = time.Unix(locator.DeviceTimestampAsInt/1000, 1000000*(locator.DeviceTimestampAsInt%1000))
+		locator.DeviceTimestamp = time.Unix(locator.DeviceTimestampAsInt / 1000, 1000000 * (locator.DeviceTimestampAsInt % 1000))
 		locator.GetRelativeSpeedDistance(db)
 
 		_, err = db.Exec("insert into locations (timestamp,devicetimestamp,latitude,longitude,accuracy,gsmtype,wifissid,distance) values ($1,$2,$3,$4,$5,$6,$7,$8)", time.Now(), &locator.DeviceTimestamp, &locator.Latitude, &locator.Longitude, &locator.Accuracy, &locator.GSMType, &locator.WifiSSID, &locator.Distance)
@@ -207,6 +207,7 @@ func LocatorHandler(c *gin.Context) {
 			}
 			_, err = tx.Exec("update locations set geocoding=$1 where id=$2", location.Geocoding, id)
 			if err != nil {
+				log.Printf("Location that caused fail is: %s", location.Geocoding)
 				tx.Rollback()
 				InternalError(err)
 			}
@@ -276,8 +277,8 @@ func DistanceOnUnitSphere(lat1 float64, long1 float64, lat2 float64, long2 float
 	// sin phi sin phi' cos(theta-theta') + cos phi cos phi'
 	// distance = rho * arc length
 
-	cos := (math.Sin(phi1)*math.Sin(phi2)*math.Cos(theta1-theta2) +
-		math.Cos(phi1)*math.Cos(phi2))
+	cos := (math.Sin(phi1) * math.Sin(phi2) * math.Cos(theta1 - theta2) +
+	math.Cos(phi1) * math.Cos(phi2))
 
 	cos = math.Max(math.Min(cos, 1.0), -1.0)
 

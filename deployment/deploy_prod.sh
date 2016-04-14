559dd878d3e026f6facbaf7f872f09a07777bcef
growse <github@growse.com>
2016-04-14 22:17:58 +0100

    Updated deploy script

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,2 +1,3 @@
 scp www.growse.com www-data@www.growse.com:/tmp/growse-web-go
-ssh www-data@www.growse.com "sudo supervisorctl stop www.growse.com-golang && mv /tmp/growse-web-go /var/www/growse-web-go/growse-web-go && cd /var/www/growse-web-src/ && git pull && cd static && grunt && sudo supervisorctl start www.growse.com-golang"
+scp assets.tgz www-data@www.growse.com:/tmp/growse-web-assets.tgz
+ssh www-data@www.growse.com "sudo supervisorctl stop www.growse.com-golang && mv /tmp/growse-web-go /var/www/growse-web-go/growse-web-go && mv /tmp/growse-web-assets.tgz /var/www/growse-web-go && cd /var/www/growse-web-go/ && tar -zxvf growse-web-assets.tgz && sudo supervisorctl start www.growse.com-golang"

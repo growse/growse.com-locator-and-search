0a98e3b094ae7cd068cb8c53ed66ebfb15f4cac0
growse <github@growse.com>
2016-05-02 11:09:18 +0100

    Deploy templates too

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
 scp www.growse.com www-data@www.growse.com:/tmp/growse-web-go
 scp static/assets.tgz www-data@www.growse.com:/tmp/growse-web-assets.tgz
-ssh www-data@www.growse.com "sudo systemctl stop www-growse-com && mv /tmp/growse-web-go /var/www/growse-web/app/growse-web-go && mv /tmp/growse-web-assets.tgz /var/www/growse-web/static/ && cd /var/www/growse-web/static/ && tar -zxvf growse-web-assets.tgz && sudo systemctl start www-growse-com"
+scp templates/templates.tgz www-data@www.growse.com:/tmp/growse-web-templates.tgz
+ssh www-data@www.growse.com "sudo systemctl stop www-growse-com && mv /tmp/growse-web-go /var/www/growse-web/app/growse-web-go && mv /tmp/growse-web-assets.tgz /var/www/growse-web/static/ && cd /var/www/growse-web/static/ && tar -zxvf growse-web-assets.tgz && mv /tmp/growse-web-templates.tgz /var/www/growse-web/templates/ && cd /var/www/growse-web/templates/ && tar -zxvf growse-web-templates.tgz && sudo systemctl start www-growse-com"

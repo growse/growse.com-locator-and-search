0a98da94eee91bc3ba2381bc1602f76088ac49b9
growse <github@growse.com>
2016-05-20 22:11:40 +0800

    SQL fail

--- databasemigrations/05_create_index_on_locations_devicetimestamp_year_part.sql
+++ databasemigrations/05_create_index_on_locations_devicetimestamp_year_part.sql
@@ -1,2 +1,2 @@
-drop index location_year_idx if exists;
+drop index if exists location_year_idx;
 create index location_year_idx on locations (date_part('year'::text, date(devicetimestamp at time zone 'UTC')));

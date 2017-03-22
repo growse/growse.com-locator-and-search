8f207e2e06fe4210f734e9a2858026c0835e7040
growse <github@growse.com>
2017-03-22 18:16:33 +0000

    Added perf number log to location db calls

--- location.go
+++ location.go
@@ -28,6 +28,7 @@ type Location struct {
 
 func GetLastLoction() (*Location, error) {
 	var location Location
+	defer timeTrack(time.Now(), "GetLocation")
 	err := db.QueryRow("select geocoding, latitude,longitude,devicetimestamp from locations where geocoding is not null order by devicetimestamp desc limit 1").Scan(&location.Geocoding, &location.Latitude, &location.Longitude, &location.Timestamp)
 	return &location, err
 }
@@ -37,6 +38,7 @@ Definately returns mph. Hence 2.236blahblah
 */
 func GetAverageSpeed() (float64, error) {
 	var speed float64
+	defer timeTrack(time.Now(), "GetAverageSpeed")
 	err := db.QueryRow("select 2.23693629*avg(speed) from (select distance/(extract(epoch from (devicetimestamp - lag(devicetimestamp) over (order by devicetimestamp asc)))::float) as speed from locations where date_part('year'::text, date(devicetimestamp at time zone 'UTC')) = $1) a;", time.Now().UTC().Year()).Scan(&speed)
 	if err != nil {
 		return 0, err
@@ -49,6 +51,7 @@ In miles.
 */
 func GetTotalDistance() (float64, error) {
 	var distance float64
+	defer timeTrack(time.Now(), "GetTotalDistance")
 	err := db.QueryRow("select 0.000621371192*sum(distance) from locations where date_part('year'::text, date(devicetimestamp at time zone 'UTC')) = $1;", time.Now().UTC().Year()).Scan(&distance)
 	if err != nil {
 		return 0, err

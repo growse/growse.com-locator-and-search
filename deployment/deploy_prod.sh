bccac1741c855aa70161a09fb5b38d499ab96204
growse <github@growse.com>
2018-01-25 20:51:06 +0000

    Switch systemd user

--- deployment/deploy_prod.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-scp www.growse.com www-data@www.growse.com:/tmp/growse-web-go
-scp templates/templates.tgz www-data@www.growse.com:/tmp/growse-web-templates.tgz
-scp databasemigrations/databasemigrations.tgz www-data@www.growse.com:/tmp/growse-web-databasemigrations.tgz
-ssh www-data@www.growse.com "mv /tmp/growse-web-databasemigrations.tgz /var/www/growse-web/databasemigrations/ && cd /var/www/growse-web/databasemigrations/ && tar -zxvf growse-web-databasemigrations.tgz && sudo systemctl stop www-growse-com && mv /tmp/growse-web-go /var/www/growse-web/app/growse-web-go && sudo systemctl start www-growse-com && cd /var/www/growse-web/static/ && mv /tmp/growse-web-templates.tgz /var/www/growse-web/templates/ && cd /var/www/growse-web/templates/ && tar -zxvf growse-web-templates.tgz"

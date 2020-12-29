1f5b7a048467583c9ee49d4fbc9d4c498ea8a0a1
Andrew Rowson <github@growse.com>
2020-12-29 13:16:08 +0000

    Added ot frontend config

--- routing.go
+++ routing.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	_ "time"
-
+	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,7 +14,30 @@ func BuildRoutes(router *gin.Engine) {
 			c.Redirect(301, "/where/ui/")
 		})
 		//authorized.GET("", PingHandler)
-		authorized.Static("ui", "/var/www/owntracks-frontend")
+		authorized.GET("ui/config/config.js",func(c *gin.Context) {
+			c.String(200,`
+var yesterday = new Date(new Date().getTime() - (24 * 60 * 60 * 1000));
+window.owntracks = window.owntracks || {};
+window.owntracks.config = {
+  api: {
+	baseUrl: "https://www.growse.com/where/location",
+	fetchOptions: {
+			credentials: "include"
+	}
+  },
+  map: {
+	center: {
+	  lat: 53.67,
+	  lng: -1.58
+	}
+  },
+  startDateTime: yesterday,
+  verbose: true
+};
+`)
+		})
+		authorized.Use(static.Serve("ui", static.LocalFile("/var/www/owntracks-frontend",false)))
+
 		otRecorderAPI := authorized.Group("data")
 		{
 			restAPI := otRecorderAPI.Group("api/0")

032268781cdb83ae211e56e6fa3574f882344507
Andrew Rowson <github@growse.com>
2020-12-29 15:33:04 +0000

    Moved static middleware for OT frontend to outer scope

--- routing.go
+++ routing.go
@@ -1,42 +1,43 @@
 package main
 
 import (
-	_ "time"
 	"github.com/gin-contrib/static"
+	_ "time"
+
 	"github.com/gin-gonic/gin"
 )
 
+const (
+	owntracksFrontendDir    = "/Users/andrew/Projects/frontend/dist"
+	owntracksFrontendConfig = `var yesterday = new Date(new Date().getTime() - (24 * 60 * 60 * 1000));
+window.owntracks = window.owntracks || {};
+window.owntracks.config = {
+  api: {
+       baseUrl: "https://www.growse.com/where/data",
+       fetchOptions: { credentials: "include" }
+  },
+  map: {
+       center: {
+         lat: 53.67,
+         lng: -1.58
+       }
+  },
+  startDateTime: yesterday,
+  verbose: true
+};`
+)
+
 func BuildRoutes(router *gin.Engine) {
+	router.Use(static.ServeRoot("/where/ui/", owntracksFrontendDir))
 	authorized := router.Group("/where/")
 	authorized.Use(AuthRequired())
 	{
 		authorized.GET("", func(c *gin.Context) {
 			c.Redirect(301, "/where/ui/")
 		})
-		//authorized.GET("", PingHandler)
-		authorized.GET("ui/config/config.js",func(c *gin.Context) {
-			c.String(200,`
-var yesterday = new Date(new Date().getTime() - (24 * 60 * 60 * 1000));
-window.owntracks = window.owntracks || {};
-window.owntracks.config = {
-  api: {
-	baseUrl: "https://www.growse.com/where/location",
-	fetchOptions: {
-			credentials: "include"
-	}
-  },
-  map: {
-	center: {
-	  lat: 53.67,
-	  lng: -1.58
-	}
-  },
-  startDateTime: yesterday,
-  verbose: true
-};
-`)
+		authorized.GET("ui/config/config.js", func(c *gin.Context) {
+			c.String(200, owntracksFrontendConfig)
 		})
-		authorized.Use(static.Serve("ui", static.LocalFile("/var/www/owntracks-frontend",false)))
 
 		otRecorderAPI := authorized.Group("data")
 		{
@@ -56,16 +57,7 @@ window.owntracks.config = {
 		}
 	}
 	router.GET("/oauth2callback", OauthCallback)
-
 	router.POST("/search/", BleveSearchQuery)
 	router.GET("/location/", LocationHandler)
 	router.HEAD("/location/", LocationHeadHandler)
-
-}
-
-func PingHandler(c *gin.Context) {
-	url := c.Request.URL
-	url.Host = "tracker.growse.com"
-	url.Path = "/"
-	c.Redirect(302, url.String())
 }
